9c248c1040f9b8923ab93f18ae6ace47b310d2bd
Kris Lamote <kris.lamote@it-kitchen.be>
2019-10-02 17:47:15 +0200

    helpers: fix endless loop in "range"

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,24 @@
 // generate a range based on start & stop params ( range(2, 7) => [2, 3, 4, 5, 6, 7] )
-export const range = (start, stop) => {
-  let rangeArr = [];
-  for (let i = start; i < stop++; i++) {
-    rangeArr.push(start++);
-  }
-  return range;
+export const buggedRange = (start, stop) => {
+    let rangeArr = [];
+    for (let i = start; i < stop++; i++) {
+        // having stop++ inside the evaluation increments stop with every stop
+        // leading to an endless loop
+        console.log(i, start, stop, rangeArr);
+        rangeArr.push(start++);
+    }
+    return range;
+};
+
+export const range = (start, end) => {
+    return Array(end - start + 1)
+        .fill()
+        .map((_, idx) => start + idx);
 };
 
 // pickOne(arr): returns a random element from the provided array
 export const pickOne = arr => {
-  return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 /**
@@ -18,12 +27,12 @@ export const pickOne = arr => {
  * without duplicates
  */
 export const pickSome = (arr, amount) => {
-  const picked = [];
-  for (let i = 0; i < amount; i++) {
-    const idx = Math.floor(Math.random() * arr.length);
-    const elements = arr.splice(idx, 1);
-    picked.push(elements[0]);
-  }
+    const picked = [];
+    for (let i = 0; i < amount; i++) {
+        const idx = Math.floor(Math.random() * arr.length);
+        const elements = arr.splice(idx, 1);
+        picked.push(elements[0]);
+    }
 
-  return picked;
+    return picked;
 };
