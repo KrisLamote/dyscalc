6527d43e61bdaddb7367f49b14bfaea8b9dc70b0
Kris Lamote <kris.lamote@it-kitchen.be>
2019-10-02 17:18:09 +0200

    merge the 2 helpers, add pickSome

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,29 @@
-export function pickOne(arr) {
+// generate a range based on start & stop params ( range(2, 7) => [2, 3, 4, 5, 6, 7] )
+export const range = (start, stop) => {
+  let rangeArr = [];
+  for (let i = start; i < stop++; i++) {
+    rangeArr.push(start++);
+  }
+  return range;
+};
 
-    return arr[Math.floor(Math.random() * arr.length)];
+// pickOne(arr): returns a random element from the provided array
+export const pickOne = arr => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
-}
+/**
+ * pickSome(arr, amount)
+ * returns amount random elements from the provided array
+ * without duplicates
+ */
+export const pickSome = (arr, amount) => {
+  const picked = [];
+  for (let i = 0; i < amount; i++) {
+    const idx = Math.floor(Math.random() * arr.length);
+    const elements = arr.splice(idx, 1);
+    picked.push(elements[0]);
+  }
+
+  return picked;
+};
