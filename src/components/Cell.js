24b91b7ef1ce5b1a7cd6b79f9dc34aa1fbc1c5a5
Junaid <junaid.ali@zeropoint.it>
2019-10-04 17:41:22 +0500

    basic fade in added to cells

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { pickOne, range } from '../helpers';
+import { CSSTransition } from "react-transition-group";
 
 const Cell = ({ targets }) => {
     const target = pickOne(targets);
     const term = pickOne(range(1, target - 1));
 
     return (
+        <CSSTransition
+            in={true}
+            appear={true}
+            timeout={1000}
+            classNames="fade"
+        >
         <div className="col-md-3 cell">
             <div className="cell-content">{`${term} + ${target - term}`}</div>
         </div>
+        </CSSTransition>
     );
 };
 
