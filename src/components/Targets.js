b92a476c8b95569a3e495aadfeb6f3d0c94080ec
Kris Lamote <kris.lamote@hr-rail.be>
2019-10-08 13:02:21 +0200

    reset, also affects Targets

--- src/components/Targets.js
+++ src/components/Targets.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
@@ -26,6 +26,13 @@ const Targets = ({ setCurrent, targets }) => {
             </div>
         );
     };
+
+    useEffect(
+        () => {
+            setActiveTarget(null);
+        },
+        [targets]
+    );
 
     return (
         <div className="row justify-content-md-center no-gutters">
