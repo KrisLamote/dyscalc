9ceadb58db66537d8625e5270140029a8ec97393
eduqg <eduardoqgomes@gmail.com>
2019-10-10 17:56:45 -0300

    Fading feedback to simulate loading targets

--- src/components/App.js
+++ src/components/App.js
@@ -11,20 +11,31 @@ const App = () => {
     const [targets, setTargets] = useState(generateTargets());
     const [current, setCurrent] = useState({ idx: null, value: null });
     const [showErrors, setShowErrors] = useState(false);
+    const [fading, setFading] = useState(true);
 
     useEffect(() => {
         setShowErrors(false);
     }, [targets]);
 
+    const sleep = m => new Promise(r => setTimeout(r, m))
+
+    async function handleReset() {
+      setFading(false);
+      await sleep(1000);
+      setTargets(generateTargets());
+      await sleep(300);
+      setFading(true);
+    }
+
     return (
       <div className="container app">
         <Header className="app__header"/>
         <div className="app__body">
-          <div className="app__body__board">
+          <div className={`app__body__board ${fading ? 'fadein' : 'fadeout'}`}>
             <Board  current={current} showErrors={showErrors} targets={targets} />
           </div>
           <div className="app__body__controls">
-            <div className="app__body__controls__targets">
+            <div className={`app__body__controls__targets ${fading ? 'fadein' : 'fadeout'}`} >
               <Targets targets={targets} setCurrent={setCurrent} />
             </div>
             <div className="app__body__controls__actions">
@@ -32,7 +43,7 @@ const App = () => {
                 showErrors={showErrors}
                 toggle={() => setShowErrors(!showErrors)}
               />
-              <Reset onClick={() => setTargets(generateTargets())} />
+              <Reset onClick={() => handleReset()} />
             </div>
           </div>
         </div>
