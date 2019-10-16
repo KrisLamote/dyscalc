ee5366f5c7f3ec94283c91187d66bfe3c454f75d
komura <h.komura2@gmail.com>
2019-10-16 02:21:48 -0300

    Add branzilian-portuguese translations

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,18 @@ import en from "./translations/en";
 import it from "./translations/it";
 import ro from "./translations/ro";
 import ru from "./translations/ru";
+import ptBR from "./translations/pt-br";
 
 const language = navigator.language.split(/[-_]/)[0];
 
-export const appLocales = ["en", "it", "ro", "ru"];
+export const appLocales = ["en", "it", "ro", "ru", "pt-BR"];
 
 export const messages = {
     en,
     it,
     ro,
     ru,
+    ptBR,
 };
 
 export default language;
