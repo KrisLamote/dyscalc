86617b62818e1c58ea45c3489bbf219bab0b2d1e
manikkalai-kannan <38786205+manikkalai-kannan@users.noreply.github.com>
2019-10-19 20:25:53 +0530

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -5,10 +5,11 @@ import ru from "./translations/ru";
 import ptBR from "./translations/pt-br";
 import ka from "./translations/ka";
 import tr from "./translations/tr";
+import ta from "./translations/ta";
 
 const language = navigator.language.split(/[-_]/)[0];
 
-export const appLocales = ["en", "it", "ro", "ru", "pt-BR", "ka", "tr"];
+export const appLocales = ["en", "it", "ro", "ru", "pt-BR", "ka", "tr", "ta"];
 
 export const messages = {
     en,
@@ -17,7 +18,8 @@ export const messages = {
     ru,
     ptBR,
     ka,
-    tr
+    tr,
+    ta
 };
 
 export default language;
