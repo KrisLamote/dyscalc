a1a60d660d723213eff7f13ecc18cf3ee87a635b
Kris Lamote <kris.lamote@it-kitchen.be>
2019-10-21 19:10:55 +0200

    add rule exceptions in line 'mostly' present coding style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-    'plugin:prettier/recommended'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-  },
+    extends: ["airbnb" /*, "plugin:prettier/recommended"*/],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        indent: ["error", 4],
+        "object-curly-newline": ["error", { consistent: true }],
+        "react/forbid-prop-types": ["error", { forbid: ["any"] }],
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/jsx-props-no-spreading": [
+            "error",
+            {
+                custom: "ignore",
+            },
+        ],
+        quotes: ["error", "double", { avoidEscape: true }],
+        "comma-dangle": 0,
+    },
 };
