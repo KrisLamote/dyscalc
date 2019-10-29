b9c1fd6642322fdf205a1c0def138becdfa3b47a
Kris Lamote <kris.lamote@it-kitchen.be>
2019-10-29 16:24:15 +0100

    move current to GameState

--- src/components/App.js
+++ src/components/App.js
@@ -39,7 +39,6 @@ function optionReducer(state, action) {
 }
 
 const App = () => {
-    // const generateTargets = () => pickSome(range(2, 10), 3).sort((a, b) => a - b);
     const [optionState, optionDispatch] = useReducer(optionReducer, initialOptionState);
     const generateTargets = useCallback(() => {
         return pickSome(range(2, optionState.upto), optionState.target).sort(
@@ -47,7 +46,6 @@ const App = () => {
         );
     }, [optionState.upto, optionState.target]);
     const [targets, setTargets] = useState(generateTargets());
-    const [current, setCurrent] = useState({ idx: null, value: null });
     const [showErrors, setShowErrors] = useState(false);
     const [showOptions, setShowOptions] = useState(false);
 
@@ -63,43 +61,54 @@ const App = () => {
         <div className="container app">
             <Header className="app__header" />
             <GameState total={16}>
-                {({ onCorrect, onIncorrect, onReset }) => (
-                    <div className="app__body">
-                        <div className="app__body__board">
-                            <Board
-                                current={current}
-                                showErrors={showErrors}
-                                targets={targets}
-                                onCorrect={onCorrect}
-                                onIncorrect={onIncorrect}
-                            />
-                            <OptionsContainer
-                                showOptions={showOptions}
-                                backToGame={() => setShowOptions(!showOptions)}
-                                optionState={optionState}
-                                optionDispatch={optionDispatch}
-                            />
-                        </div>
-                        <div className="app__body__controls">
-                            <div className="app__body__controls__targets">
-                                <Targets targets={targets} setCurrent={setCurrent} />
-                            </div>
-                            <div className="app__body__controls__actions">
-                                <Verify
+                {state => {
+                    const {
+                        current,
+                        onCorrect,
+                        onIncorrect,
+                        onReset,
+                        setCurrent,
+                    } = state;
+                    return (
+                        <div className="app__body">
+                            <div className="app__body__board">
+                                <Board
+                                    current={current}
                                     showErrors={showErrors}
-                                    toggle={() => setShowErrors(!showErrors)}
+                                    targets={targets}
+                                    onCorrect={onCorrect}
+                                    onIncorrect={onIncorrect}
                                 />
-                                <Reset
-                                    onClick={() => {
-                                        onReset();
-                                        setTargets(generateTargets());
-                                    }}
+                                <OptionsContainer
+                                    showOptions={showOptions}
+                                    backToGame={() => setShowOptions(!showOptions)}
+                                    optionState={optionState}
+                                    optionDispatch={optionDispatch}
                                 />
-                                <Options onClick={() => setShowOptions(!showOptions)} />
+                            </div>
+                            <div className="app__body__controls">
+                                <div className="app__body__controls__targets">
+                                    <Targets targets={targets} setCurrent={setCurrent} />
+                                </div>
+                                <div className="app__body__controls__actions">
+                                    <Verify
+                                        showErrors={showErrors}
+                                        toggle={() => setShowErrors(!showErrors)}
+                                    />
+                                    <Reset
+                                        onClick={() => {
+                                            onReset();
+                                            setTargets(generateTargets());
+                                        }}
+                                    />
+                                    <Options
+                                        onClick={() => setShowOptions(!showOptions)}
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )}
+                    );
+                }}
             </GameState>
         </div>
     );
--- src/components/GameState.js
+++ src/components/GameState.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const GameState = ({ total, children }) => {
+    const [current, setCurrent] = useState({ idx: null, value: null });
     const [items, setItems] = useState(total);
     const onCorrect = () => setItems(items - 1);
     const onIncorrect = () => setItems(items + 1);
@@ -12,7 +13,7 @@ const GameState = ({ total, children }) => {
             {items === 0 ? (
                 <p className="text-white bg-dark text-center">Game Completed!</p>
             ) : null}
-            {children({ onCorrect, onIncorrect, onReset })}
+            {children({ current, onCorrect, onIncorrect, onReset, setCurrent })}
         </>
     );
 };
