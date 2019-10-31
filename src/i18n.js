45c7acde4b0807395615b3a29975512a7fecfab4
KrisLamote <kris.lamote@gmail.com>
2019-10-31 13:32:28 +0100

    fix: pr, pr-PT & pr-BR use pt translations

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@ import en from "./translations/en";
 import it from "./translations/it";
 import ro from "./translations/ro";
 import ru from "./translations/ru";
-import ptBR from "./translations/pt-br";
+import pt from "./translations/pt";
 import ka from "./translations/ka";
 import tr from "./translations/tr";
 import ur from "./translations/ur";
@@ -10,7 +10,7 @@ import ta from "./translations/ta";
 
 const language = navigator.language.split(/[-_]/)[0];
 
-export const appLocales = ["en", "it", "ro", "ru", "pt-BR", "ka", "tr", "ur", "ta"];
-export const messages = { en, it, ka, ptBR, ro, ru, ta, tr, ur };
+export const appLocales = ["en", "it", "ro", "ru", "pt", "ka", "tr", "ur", "ta"];
+export const messages = { en, it, ka, pt, ro, ru, ta, tr, ur };
 
 export default language;
