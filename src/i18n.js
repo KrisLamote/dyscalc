7048e33a3c8d466c64d0fd2b5c882e78f28ad89e
KrisLamote <kris.lamote@it-kitchen.be>
2019-10-31 13:37:27 +0100

    Fix portuguese (#67)
    * fix: pr, pr-PT & pr-BR use pt translations
    * chore: sort lang props

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,16 @@
 import en from "./translations/en";
 import it from "./translations/it";
+import ka from "./translations/ka";
+import pt from "./translations/pt";
 import ro from "./translations/ro";
 import ru from "./translations/ru";
-import ptBR from "./translations/pt-br";
-import ka from "./translations/ka";
+import ta from "./translations/ta";
 import tr from "./translations/tr";
 import ur from "./translations/ur";
-import ta from "./translations/ta";
 
 const language = navigator.language.split(/[-_]/)[0];
 
-export const appLocales = ["en", "it", "ro", "ru", "pt-BR", "ka", "tr", "ur", "ta"];
-export const messages = { en, it, ka, ptBR, ro, ru, ta, tr, ur };
+export const appLocales = ["en", "it", "ka", "pt", "ro", "ru", "ta", "tr", "ur"];
+export const messages = { en, it, ka, pt, ro, ru, ta, tr, ur };
 
 export default language;
