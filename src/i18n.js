f2a1ece67b777fb034d1ee9a6fa863ffa710c695
Rohan Khanna <khanna.rohan1993@gmail.com>
2019-11-01 01:52:38 +0530

    spanish added to list of locales

--- src/i18n.js
+++ src/i18n.js
@@ -8,10 +8,11 @@ import ta from "./translations/ta";
 import tr from "./translations/tr";
 import ur from "./translations/ur";
 import hu from "./translations/hu";
+import es from "./translations/es";
 
 const language = navigator.language.split(/[-_]/)[0];
 
-export const appLocales = ["en", "it", "ka", "pt", "ro", "ru", "ta", "tr", "ur", "hu"];
-export const messages = { en, it, ka, pt, ro, ru, ta, tr, ur, hu };
+export const appLocales = ["en", "it", "ka", "pt", "ro", "ru", "ta", "tr", "ur", "hu", "es"];
+export const messages = { en, it, ka, pt, ro, ru, ta, tr, ur, hu, es };
 
 export default language;
